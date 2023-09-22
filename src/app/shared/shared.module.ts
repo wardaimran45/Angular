62733561bd72cd5b11006b57128c85f638279f34
Warda Imran <warda.imran45@gmail.com>
2023-09-22 18:40:39 +0500

    Module 13 half

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './alert/alert.component';
+import { EventBlockerDirective } from './directives/event-blocker.directive';
 
 
 
@@ -16,7 +17,8 @@ import { AlertComponent } from './alert/alert.component';
     TabsContainerComponent,
     TabComponent,
     InputComponent,
-    AlertComponent
+    AlertComponent,
+    EventBlockerDirective
    
   ],
   imports: [
@@ -29,7 +31,8 @@ import { AlertComponent } from './alert/alert.component';
     TabsContainerComponent,
     TabComponent,
     InputComponent,
-    AlertComponent
+    AlertComponent,
+    EventBlockerDirective
   ],
   // providers: [
   //   ModalService
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit{
+  isDragover = false
+  file: File | null = null 
+  nextStep = false
+
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  storeFile($event: Event){
+    this.isDragover = false
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if(!this.file || this.file.type !== 'video/mp4'){
+      return 
+
+    }
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')    //This expression removes the file extension and replaces it with empty string
+    )
+    this.nextStep = true
+  }
+  uploadFile(){
+    console.log("File Uploaded");
+    
+  }
 
 }
